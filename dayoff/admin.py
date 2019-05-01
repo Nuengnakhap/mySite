2d46a95fa4cdbcd608c593739a978fa4e3b16f34
Onez <nueng1569@hotmail.com>
2019-05-01 09:44:07 +0700

    Exercise 6 [add readonly_fields]
    Signed-off-by: nuengnakhap <60070005@kmitl.ac.th>

--- dayoff/admin.py
+++ dayoff/admin.py
@@ -8,4 +8,11 @@ class DayOffAdmin(admin.ModelAdmin):
     list_filter = ['created_by', 'date_start', 'date_end', 'approve_status']
     list_per_page = 20
 
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_staff:
+            if request.user.is_superuser:
+                return []
+            else:
+                return ['created_by', 'type', 'reason', 'date_start', 'date_end']
+
 admin.site.register(DayOff, DayOffAdmin)
